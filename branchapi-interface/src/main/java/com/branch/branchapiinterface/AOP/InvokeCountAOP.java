ecc6e5bc606f52dba1d0656ca0fcf08aba895499
“Ee1ex” <1787592477@qq.com>
2024-08-09 03:55:36 +0800

    ```markdownfeat(gateway): 添加基于Spring Cloud Gateway的路由配置- 配置了一个路由到yupi.icu的路径 - 启动类BranchapiGatewayApplication中添加了RouteLocatorBuilder的bean定义 - 在application.yml中指定了网关服务的端口为8090
    BREAKING CHANGE: 无```

---
+++ branchapi-interface/src/main/java/com/branch/branchapiinterface/AOP/InvokeCountAOP.java
@@ -0,0 +1,20 @@
+package com.branch.branchapiinterface.AOP;
+
+
+import javax.annotation.Resource;
+
+/**
+ * 调用次数统计切面
+ */
+public class InvokeCountAOP {
+
+//    @Resource
+//    private InvokeCountService invokeCountService;
+//
+//    public void invokeCount(String methodName, String userId) {
+//        invokeCountService.invokeCount(methodName, userId);
+//    }
+//    public void invokeCount(String methodName, String userId, String ip) {
+//        invokeCountService.invokeCount(methodName, userId, ip);
+//    }
+}
