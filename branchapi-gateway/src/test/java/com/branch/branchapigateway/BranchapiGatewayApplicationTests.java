2942ebb9dda2e5eb6a045706284316650f1ff333
“Ee1ex” <1787592477@qq.com>
2024-08-09 03:55:36 +0800

    ```markdownfeat(gateway): 添加基于Spring Cloud Gateway的路由配置- 配置了一个路由 - 启动类BranchapiGatewayApplication中添加了RouteLocatorBuilder的bean定义 - 在application.yml中指定了网关服务的端口为8090
    BREAKING CHANGE: 无```

---
+++ branchapi-gateway/src/test/java/com/branch/branchapigateway/BranchapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.branch.branchapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchapiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
